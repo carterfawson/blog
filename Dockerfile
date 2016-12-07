d7bad5c41e3aab5b84ae948795b25751255ae29f
Carter Fawson <carteraaronfawson@gmail.com>
2016-12-06 22:32:11 -0600

    WIP

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM node:7.2.0-alpine
 WORKDIR /app
 COPY . .
 RUN npm install --production
+EXPOSE 3000
 
 # if we don't use this specific form, SIGINT/SIGTERM doesn't get forwarded
 CMD node index.js
---
+++ app/blog-editModal/blog-editModal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'blog-editModal',
+    templateUrl: 'app/blog-editModal/blog-editModal.component.html'
+})
+
+export class BlogEditModalComponent {
+    
+}
